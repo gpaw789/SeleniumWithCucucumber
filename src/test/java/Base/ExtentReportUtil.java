48eeca4be202f9d206206630aa94364b69bca975
gpaw789 <georgepaw789@hotmail.com>
2019-05-05 19:15:00 +1000

    Update ExtentReportUtil.java

--- src/test/java/Base/ExtentReportUtil.java
+++ src/test/java/Base/ExtentReportUtil.java
@@ -35,8 +35,8 @@ public class ExtentReportUtil extends BaseUtil {
     }
 
     public void ExtentReportScreenshot() throws IOException {
-        
-        var scr = ((TakesScreenshot)Driver).getScreenshotAs(OutputType.FILE);
+        var scr = ""
+        scr = ((TakesScreenshot)Driver).getScreenshotAs(OutputType.FILE);
         Files.copy(scr.toPath(), new File(reportLocation + "screenshot.png").toPath());
         scenarioDef.fail("details").addScreenCaptureFromPath(reportLocation + "screenshot.png");
     }
